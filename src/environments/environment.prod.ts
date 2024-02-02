2aace44fb110cb795c1a14a8f01c0240a0d6eb59
Ladoxer <ladderdoxer@gmail.com>
2024-02-02 16:45:32 +0530

    environment fix

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environments = {
+  production: true,
+  apiUrl: 'https://voxvortex-backend.onrender.com/api/',
+  backendUrl: 'https://voxvortex-backend.onrender.com/'
+}
---
+++ src/environments/environments.ts
@@ -0,0 +1,5 @@
+export const environments = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  backendUrl: 'http://localhost:3000/'
+}
